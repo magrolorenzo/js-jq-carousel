757e01147216e40fcf8fd15af1d3138d6451cd10
Lorenzo Magro <magrolorenzo@gmail.com>
2020-10-27 16:42:14 +0100

    creazione main.js - non carica lo script

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
+    alert('SCRIPT OK');
+
+    $('.right-side i').click( function() {
+
+        //  Memorizzo l'immagine corrente
+        var current_img = $('img.is-visible');
+
+        // Tolgo all'immagine corrente la classe is-visible
+        current_img.removeClass('is-visible');
+
+        if(current_img.next().length){
+            // Assegno all'immagine successiva la classe is-visible
+            current_img.next().addClass('is-visible');
+        } else {
+            $('.slides img:first-child').addClass('is-visible');
+        }
+
+    })
 
 });
